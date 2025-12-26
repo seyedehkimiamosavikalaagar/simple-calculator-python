aca3953df05ab772365d2bfb8ab89ef5285cd16c
seyedehkimiamosavikalaagar <seyedehkimiamosavikalaagar@gmail.com>
2025-12-26 09:17:22 +0330

    decision-money
    add simple financial decision example

---
+++ decision_money.py
@@ -0,0 +1,35 @@
+{
+  "metadata": {
+    "kernelspec": {
+      "name": "xpython",
+      "display_name": "Python 3.13 (XPython)",
+      "language": "python"
+    },
+    "language_info": {
+      "file_extension": ".py",
+      "mimetype": "text/x-python",
+      "name": "python",
+      "version": "3.13.1"
+    }
+  },
+  "nbformat_minor": 5,
+  "nbformat": 4,
+  "cells": [
+    {
+      "id": "ea53c79c-9876-4dac-a54e-4e1c74dbb201",
+      "cell_type": "code",
+      "source": "Money = 850000 #مقدار پول (تومان)\nif Money >= 1000000:\n    print(\"میتونی خرید کنی\")\nelse:\n    print(\"پولت کافی نیست\")",
+      "metadata": {
+        "trusted": true
+      },
+      "outputs": [
+        {
+          "name": "stdout",
+          "output_type": "stream",
+          "text": "پولت کافی نیست\n"
+        }
+      ],
+      "execution_count": 1
+    }
+  ]
+}
